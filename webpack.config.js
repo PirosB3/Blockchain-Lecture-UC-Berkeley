cb53b7c743ea4c436e5f2f3e3f3c679f2d76ec69
Daniel Pyrathon <daniel@0x.org>
2020-03-25 20:14:29 -0700

    added webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
     mode: 'development',
     entry: './src/index.ts',
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        port: 9000
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build'),
