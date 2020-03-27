8dd309c44aaeb392af2507b92137024e83de3c89
Daniel Pyrathon <daniel@0x.org>
2020-03-26 19:09:00 -0700

    refacxtor

--- src/index.ts
+++ src/index.ts
@@ -6,63 +6,71 @@ import { getContractAddressesForChainOrThrow, ChainId } from '@0x/contract-addre
 import { MetamaskSubprovider, Web3ProviderEngine, RPCSubprovider } from '@0x/subproviders'
 import {BigNumber } from '@0x/utils';
 import axios from 'axios';
-import { KOVAN_0x_API, FAKE_DAI, FAKE_USDC, MetamaskWindow, INFURA_RPC_URL, DEFAULT_MINT_AMOUNT, INFINITE_ALLOWANCE } from './misc';
+import { KOVAN_0x_API, FAKE_DAI, FAKE_USDC, MetamaskWindow, INFURA_RPC_URL, DEFAULT_MINT_AMOUNT, INFINITE_ALLOWANCE, linkBtnToCallback, mintTokens, DEFAULT_ALLOWANCE_AMOUNT, MAP_TOKEN_TO_NAME } from './misc';
 
-async function mintTokens(fromAddress: string, tokenAddress: string, provider: SupportedProvider, mintAmount: BigNumber = DEFAULT_MINT_AMOUNT): Promise<string> {
-    const contractInstance = new DummyERC20TokenContract(tokenAddress, provider);
-    const numDecimals = await contractInstance.decimals().callAsync();
-    const mintAmountInBaseUnits = Web3Wrapper.toBaseUnitAmount(mintAmount, numDecimals.toNumber());
-
-    const tx = await contractInstance.mint(mintAmountInBaseUnits).awaitTransactionSuccessAsync({
-        from: fromAddress,
-    });
-    return tx.transactionHash;
-}
-
+/**
+ * Returns the decimals for a token. Decimals indicates how many 0's there are to the right of the decimal point the fixed-point representation of a token.
+ * @param tokenAddress the address of the token
+ * @param provider the supported provider
+ */
 async function getDecimalsForToken(tokenAddress: string, provider: SupportedProvider): Promise<number> {
     const contractInstance = new DummyERC20TokenContract(tokenAddress, provider);
     const decimals = await contractInstance.decimals().callAsync()
     return decimals.toNumber();
 }
 
-async function performSwap(buyToken: string, sellToken: string, amountInUnitAmount: number, fromAddress: string, client: Web3Wrapper): Promise<string> {
+/**
+ * Performs a trade by requesting a quote from the 0x API, and filling that quote on the blockchain
+ * @param buyToken the token address to buy
+ * @param sellToken the token address to sell
+ * @param amountToSellUnitAmount the token amount to sell
+ * @param fromAddress the address that will perform the transaction
+ * @param client the Web3Wrapper client
+ */
+async function performSwap(buyToken: string, sellToken: string, amountToSellUnitAmount: number, fromAddress: string, client: Web3Wrapper): Promise<string> {
     // Fetch decimals
     const numDecimals = await getDecimalsForToken(sellToken, client.getProvider());
-    const sellAmountInBaseUnits = Web3Wrapper.toBaseUnitAmount(amountInUnitAmount, numDecimals);
+    const sellAmountInBaseUnits = Web3Wrapper.toBaseUnitAmount(amountToSellUnitAmount, numDecimals);
+    console.log(`Requesting 0x API to provide a quote for swapping ${sellAmountInBaseUnits} of ${MAP_TOKEN_TO_NAME[sellToken]} for ${MAP_TOKEN_TO_NAME[buyToken]}`)
 
     // Make API request
-    const response = await axios.get<TxData>(`${KOVAN_0x_API}/swap/v0/quote?buyToken=${buyToken}&sellToken=${sellToken}&sellAmount=${sellAmountInBaseUnits}&takerAddress=${fromAddress}`);
+    const httpResponse = await axios.get<TxData>(`${KOVAN_0x_API}/swap/v0/quote?buyToken=${buyToken}&sellToken=${sellToken}&sellAmount=${sellAmountInBaseUnits}&takerAddress=${fromAddress}`);
+    const apiResponse = httpResponse.data;
+    console.log(`Received a response from the 0x API:`)
+    console.log(apiResponse);
 
+    // Perform the transaction on-chain
     const tx = await client.sendTransactionAsync({
-        ...response.data,
+        from: fromAddress,
+        to: apiResponse.to,
+        data: apiResponse.data,
+        gas: apiResponse.gas,
+        gasPrice: apiResponse.gasPrice,
+        value: apiResponse.value,
     })
     const receipt = await client.awaitTransactionSuccessAsync(tx)
+    console.log(`Transaction ${receipt.transactionHash} was mined successfully`)
     return receipt.transactionHash;
 }
 
-async function setAllowances(fromAddress: string, tokenAddress: string, provider: SupportedProvider, mintAmount: BigNumber = DEFAULT_MINT_AMOUNT): Promise<string> {
+/**
+ * Allows the 0x Exchange to pull a limited amount of funds on your behalf
+ * @param fromAddress the address that will perform the transaction
+ * @param tokenAddress the address of the token
+ * @param provider a supported provider
+ * @param mintAmount the amount to mint in unit amount
+ */
+async function setAllowances(fromAddress: string, tokenAddress: string, provider: SupportedProvider, allowanceAmount: BigNumber = DEFAULT_ALLOWANCE_AMOUNT): Promise<string> {
+    const tokenDecimals = await getDecimalsForToken(tokenAddress, provider);
     const contractInstance = new DummyERC20TokenContract(tokenAddress, provider);
     const addresses = getContractAddressesForChainOrThrow(ChainId.Kovan);
     const tx = await contractInstance.approve(
         addresses.erc20Proxy,
-        INFINITE_ALLOWANCE,
+        Web3Wrapper.toBaseUnitAmount(allowanceAmount, tokenDecimals),
     ).awaitTransactionSuccessAsync({
         from: fromAddress,
     });
     return tx.transactionHash;
-}
-
-/**
- * Links the `onclick` event of a button denoted by ID to a callback
- * @param buttonId the button ID as a string, must be unique
- * @param callback a JS function callback that is triggered with the button is pressed
- */
-function linkBtnToCallback(buttonId: string, callback: (...args: any[]) => any): void {
-    const button = document.getElementById(buttonId);
-    if (button === null) {
-        throw new Error(`Button ${buttonId} was not found`);
-    }
-    button.onclick = callback;
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -79,6 +87,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
     const [account] = await scopedWindow.ethereum.enable();
     scopedWindow.ethereum.on('accountsChanged', () => location.reload());
+    if (scopedWindow.web3 === undefined) {
+        throw new Error('Web3 not defined, please install and unlock Metamask');
+    }
 
     const providerEngine = new Web3ProviderEngine();
     providerEngine.addProvider(new MetamaskSubprovider(scopedWindow.web3.currentProvider));
@@ -87,15 +98,16 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // We initialize the Web3Wrapper, which is 0x's alternative to Web3.js library.
     const client = new Web3Wrapper(providerEngine);
+    const provider = client.getProvider();
     const chainId = await client.getChainIdAsync();
     if (chainId !== 42) {
         throw new Error(`Chain ID should be set to Kovan, it was set to ${chainId}`);
     }
 
-    linkBtnToCallback("mintDAI", () => mintTokens(account, FAKE_DAI, client.getProvider(), DEFAULT_MINT_AMOUNT));
-    linkBtnToCallback("mintUSDC", () => mintTokens(account, FAKE_USDC, client.getProvider(), DEFAULT_MINT_AMOUNT));
+    linkBtnToCallback("mintDAI", () => mintTokens(account, FAKE_DAI, provider, DEFAULT_MINT_AMOUNT));
+    linkBtnToCallback("mintUSDC", () => mintTokens(account, FAKE_USDC, provider, DEFAULT_MINT_AMOUNT));
+    linkBtnToCallback("allowDAI", () => setAllowances(account, FAKE_DAI, provider));
+    linkBtnToCallback("allowUSDC", () => setAllowances(account, FAKE_USDC, provider));
     linkBtnToCallback("swapDaiForUsdc", () => performSwap(FAKE_USDC, FAKE_DAI, 100, account, client));
     linkBtnToCallback("swapUsdcForDai", () => performSwap(FAKE_DAI, FAKE_USDC, 100, account, client));
-    linkBtnToCallback("allowDAI", () => setAllowances(account, FAKE_DAI, client.getProvider()));
-    linkBtnToCallback("allowUSDC", () => setAllowances(account, FAKE_USDC, client.getProvider()));
 });
--- src/misc.ts
+++ src/misc.ts
@@ -1,7 +1,9 @@
 import { BigNumber } from "@0x/utils";
-import { SupportedProvider } from "@0x/web3-wrapper";
+import { SupportedProvider, Web3Wrapper } from "@0x/web3-wrapper";
+import { DummyERC20TokenContract } from "@0x/contracts-erc20";
 
 export const DEFAULT_MINT_AMOUNT = new BigNumber(10_000);
+export const DEFAULT_ALLOWANCE_AMOUNT = new BigNumber(1_000_000);
 export const FAKE_DAI = '0x48178164eB4769BB919414Adc980b659a634703E';
 export const FAKE_USDC = '0x5a719Cf3E02c17c876F6d294aDb5CB7C6eB47e2F';
 export const INFINITE_ALLOWANCE = new BigNumber(2).pow(256).minus(1);
@@ -10,9 +12,15 @@ export const ZERO = new BigNumber(0);
 export const DEFAULT_GAS_PRICE = new BigNumber(5000000000);
 export const KOVAN_0x_API = 'https://kovan.api.0x.org'
 export const INFURA_RPC_URL = 'https://kovan.infura.io/v3/f98b693fe61e41ada1a82dab93a3a888'
+export const MAP_TOKEN_TO_NAME: {[key: string]: string} = {
+    [FAKE_DAI]: 'DAI',
+    [FAKE_USDC]: 'DAI',
+}
 
+// Please don't mind this interface definition, it's only needed to
+// tell TypeScript that `web3` and `ethereum` may be globally defined
 export interface MetamaskWindow {
-    web3: {
+    web3?: {
         currentProvider: SupportedProvider,
     }
     ethereum?: {
@@ -21,3 +29,37 @@ export interface MetamaskWindow {
     }
 }
 
+
+/**
+ * Links the `onclick` event of a button denoted by ID to a callback
+ * @param buttonId the button ID as a string, must be unique
+ * @param callback a JS function callback that is triggered with the button is pressed
+ */
+export function linkBtnToCallback(buttonId: string, callback: (...args: any[]) => any): void {
+    const button = document.getElementById(buttonId);
+    if (button === null) {
+        throw new Error(`Button ${buttonId} was not found`);
+    }
+    button.onclick = callback;
+}
+
+
+/**
+ * A simple utility that can be used to mint tokens. The DummyERC20Token contract exposes a special
+ * `mint()` function that can be used to create tokens out of thin air!
+ * 
+ * @param fromAddress the address receiving the tokens
+ * @param tokenAddress the address of the ERC20 token
+ * @param provider the Web3 provider
+ * @param mintAmount the amount to mint, this can be a maximum of 10,000
+ */
+export async function mintTokens(fromAddress: string, tokenAddress: string, provider: SupportedProvider, mintAmount: BigNumber = DEFAULT_MINT_AMOUNT): Promise<string> {
+    const contractInstance = new DummyERC20TokenContract(tokenAddress, provider);
+    const numDecimals = await contractInstance.decimals().callAsync();
+    const mintAmountInBaseUnits = Web3Wrapper.toBaseUnitAmount(mintAmount, numDecimals.toNumber());
+
+    const tx = await contractInstance.mint(mintAmountInBaseUnits).awaitTransactionSuccessAsync({
+        from: fromAddress,
+    });
+    return tx.transactionHash;
+}
