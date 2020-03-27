43368c49ba10ef0dd4019838c3ebe1695c518b6d
Daniel Pyrathon <daniel@0x.org>
2020-03-27 00:35:33 -0700

    added docs

--- src/exercise.ts
+++ src/exercise.ts
@@ -1,13 +1,16 @@
 import axios from 'axios';
 import { SupportedProvider, Web3Wrapper, TxData } from "@0x/web3-wrapper";import { DummyERC20TokenContract } from "@0x/contracts-erc20";import { MAP_TOKEN_TO_NAME, KOVAN_0x_API, DEFAULT_ALLOWANCE_AMOUNT } from "./misc";import { BigNumber } from "@0x/utils";import { getContractAddressesForChainOrThrow, ChainId } from "@0x/contract-addresses";
 
+const zeroExDeployedAddresses = getContractAddressesForChainOrThrow(ChainId.Kovan);
+
 
 /**
  * Returns the decimals for a token. Decimals indicates how many 0's there are to the right of the decimal point the fixed-point representation of a token.
  * @param tokenAddress the address of the token
  * @param provider the supported provider
  */
-export async function getDecimalsForToken(tokenAddress: string, provider: SupportedProvider): Promise<number> {
+export async function getDecimalsForTokenAsync(tokenAddress: string, provider: SupportedProvider): Promise<number> {
+    // Initialize a new DummyERC20TokenContract instance, and call the `decimals()` function on the contract.
     const contractInstance = new DummyERC20TokenContract(tokenAddress, provider);
     const decimals = await contractInstance.decimals().callAsync()
     return decimals.toNumber();
@@ -21,13 +24,17 @@ export async function getDecimalsForToken(tokenAddress: string, provider: Suppor
  * @param fromAddress the address that will perform the transaction
  * @param client the Web3Wrapper client
  */
-export async function performSwap(buyToken: string, sellToken: string, amountToSellUnitAmount: number, fromAddress: string, client: Web3Wrapper): Promise<string> {
-    // Fetch decimals
-    const numDecimals = await getDecimalsForToken(sellToken, client.getProvider());
+export async function performSwapAsync(buyToken: string, sellToken: string, amountToSellUnitAmount: number, fromAddress: string, client: Web3Wrapper): Promise<string> {
+    // Step #1) Convert the unit amount into base unit amount (bigint). For this to happen you need the number of decimals the token.
+    // Fetch decimals using the getDecimalsForToken(), and use Web3Wrapper.toBaseUnitAmount() to perform the conversion
+    const numDecimals = await getDecimalsForTokenAsync(sellToken, client.getProvider());
     const sellAmountInBaseUnits = Web3Wrapper.toBaseUnitAmount(amountToSellUnitAmount, numDecimals);
     console.log(`Requesting 0x API to provide a quote for swapping ${sellAmountInBaseUnits} of ${MAP_TOKEN_TO_NAME[sellToken]} for ${MAP_TOKEN_TO_NAME[buyToken]}`)
 
-    // Make API request
+    // Step #2) Make a request to the 0x API swap endpoint: https://0x.org/docs/guides/swap-tokens-with-0x-api#swap-eth-for-1-dai
+    // You can use the line below as guidance. In the example, the variable TxData contains the deserialized JSON response from the API.
+    // const httpResponse = await axios.get<TxData>(url)
+    // const txData: TxData = httpResponse.data;
     let apiResponse: TxData;
     try {
         const httpResponse = await axios.get<TxData>(`${KOVAN_0x_API}/swap/v0/quote?buyToken=${buyToken}&sellToken=${sellToken}&sellAmount=${sellAmountInBaseUnits}&takerAddress=${fromAddress}`);
@@ -39,7 +46,7 @@ export async function performSwap(buyToken: string, sellToken: string, amountToS
         return '';
     }
 
-    // Perform the transaction on-chain
+    // Step #3) You can `client.sendTransactionAsync()` to send a Ethereum transaction.
     const tx = await client.sendTransactionAsync({
         from: fromAddress,
         to: apiResponse.to,
@@ -48,6 +55,9 @@ export async function performSwap(buyToken: string, sellToken: string, amountToS
         gasPrice: apiResponse.gasPrice,
         value: apiResponse.value,
     })
+
+    // Step #4) `client.sendTransactionAsync()` returns immediately after submitting a transaction with a transaction hash. you may use `client.awaitTransactionSuccessAsync()`
+    // to block until the transaction has been mined.
     const receipt = await client.awaitTransactionSuccessAsync(tx)
     console.log(`Transaction ${receipt.transactionHash} was mined successfully`)
     return receipt.transactionHash;
@@ -61,11 +71,15 @@ export async function performSwap(buyToken: string, sellToken: string, amountToS
  * @param mintAmount the amount to mint in unit amount
  */
 export async function setAllowances(fromAddress: string, tokenAddress: string, provider: SupportedProvider, allowanceAmount: BigNumber = DEFAULT_ALLOWANCE_AMOUNT): Promise<string> {
-    const tokenDecimals = await getDecimalsForToken(tokenAddress, provider);
+    const tokenDecimals = await getDecimalsForTokenAsync(tokenAddress, provider);
+
+    // Initialize a new DummyERC20TokenContract instance, and call the `decimals()` function on the contract.
     const contractInstance = new DummyERC20TokenContract(tokenAddress, provider);
-    const addresses = getContractAddressesForChainOrThrow(ChainId.Kovan);
+
+    // In order to allow the 0x smart contracts to trade with your funds, you need to set an allowance for zeroExDeployedAddresses.erc20Proxy. This
+    // can be done using the `approve` function.
     const tx = await contractInstance.approve(
-        addresses.erc20Proxy,
+        zeroExDeployedAddresses.erc20Proxy,
         Web3Wrapper.toBaseUnitAmount(allowanceAmount, tokenDecimals),
     ).awaitTransactionSuccessAsync({
         from: fromAddress,
